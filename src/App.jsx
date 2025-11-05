aef0fdef08f171957cb40a9b7dffff9a85dcb85b
HenriqueFouquet <henrique.fouquet.labcomu@imd.ufrn.br>
2025-11-05 08:41:59 -0300

    commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,87 @@
+import { useEffect, useState } from "react";
+import AddTask from "./components/AddTask";
+import Tasks from "./components/Tasks";
+import { v4 } from "uuid";
+import Title from "./components/Title";
+
+function App() {
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem("tasks")) || []
+  );
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      // CHAMA A API
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos?_limit=10",
+        {
+          method: "GET",
+        }
+      );
+
+      // PEGAR OS DADOS QUE ELA RETORNA
+      const data = await response.json();
+
+      // ARMAZENAR ESSES DADOS NO STATE
+      setTasks(data);
+    };
+    // fetchTasks();
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      if (task.id === taskId) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+
+      return task;
+    });
+    setTasks(newTasks);
+  }
+
+  function onDeleteTaskClick(taskId) {
+    // let indexTask = 0;
+    // for (let index in tasks) {
+    //   if (tasks[index].taskId === taskId) {
+    //     indexTask = index;
+    //     break;
+    //   }
+    // }
+
+    // const newTasks = tasks.toSpliced(indexTask, 1);
+    const newTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(newTasks);
+  }
+
+  function onAddTaskSubmit(title, description) {
+    const newTask = {
+      id: v4(),
+      title,
+      description,
+      isCompleted: false,
+    };
+    // tasks.push(newTask);
+    // setTasks(tasks);
+    setTasks([...tasks, newTask]);
+  }
+
+  return (
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px] space-y-4">
+        <Title>Gerenciador de Tarefas</Title>
+        <AddTask onAddTaskSubmit={onAddTaskSubmit} />
+        <Tasks
+          tasks={tasks}
+          onTaskClick={onTaskClick}
+          onDeleteTaskClick={onDeleteTaskClick}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
